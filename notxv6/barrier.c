b939874f168b09929eb85d5f138e40b58cd05dc6
ArmeriaWang <armeriaw@outlook.com>
2021-04-26 12:09:13 +0800

    passed all

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,21 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  if (bstate.nthread == nthread) {
+    bstate.nthread = 0;
+    bstate.round++;
+    // printf("thread %ld boardcasting...\n", pthread_self() % 10);
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  else {
+    // printf("thread %ld waiting...\n", pthread_self() % 10);
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    // printf("thread %ld woke up!\n", pthread_self() % 10);
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
 }
 
 static void *
